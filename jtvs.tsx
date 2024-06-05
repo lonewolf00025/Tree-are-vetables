6e32b35a0757be80e50b9e1dd5749703d22bbecb
lonewolf00025 <149115525+lonewolf00025@users.noreply.github.com>
2024-06-05 12:26:30 -0700

    006jtvs

--- jtvs.tsx
+++ jtvs.tsx
@@ -1,16 +1,14 @@
 import {
-    HDKComponent,
-    HNode,
-    Prefab,
-    render,
-    Animation,
-    InfoPanel,
-  } from '@hiber3d/hdk-react';
-  const World = () => 
-    <HNode y={-1}>
-      <Prefab id="flat_cube_01"/>
-    </HNode>
-  ;
-  
-  render(<World />, { environment: 'sunrise_01' });
-  
+  HDKComponent,
+  HNode,
+  Prefab,
+  render,
+  Animation,
+  InfoPanel,
+} from '@hiber3d/hdk-react';
+const World = () => (
+  <HNode y={-1}>
+    <Prefab id="flat_cube_01" scaleX={50} scaleY={50} />
+  </HNode>
+);
+render(<World />, { environment: 'sunrise_01' });
