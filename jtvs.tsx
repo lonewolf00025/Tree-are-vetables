10734daa25e32a923b1b5dec11c3ff2103e2dd4e
lonewolf00025 <149115525+lonewolf00025@users.noreply.github.com>
2024-06-05 12:04:07 -0700

    004jtvs

--- jtvs.tsx
+++ jtvs.tsx
@@ -6,7 +6,11 @@ import {
     Animation,
     InfoPanel,
   } from '@hiber3d/hdk-react';
-  const World = () => <HNode y={-1}></HNode>;
+  const World = () => 
+    <HNode y={-1}>
+      <Prefab id="flat_cube_01"/>
+    </HNode>
+  ;
   
   render(<World />, { environment: 'sunrise_01' });
   
